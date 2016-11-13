42186366d7ab5963607f7f8fcb6ef3018f3e4c2a
Sophia <swoozie14.com>
2016-11-13 12:34:43 -0800

    Complete pop, contains?, find, to_s methods

--- linked_list.rb
+++ linked_list.rb
@@ -56,7 +56,7 @@ class LinkedList
   end
 
   # Returns the node at the given index
-  # Returns nil if the given index is out of bounds
+  # => returns nil if the given index is out of bounds
   def at(index)
     return nil if index < 0 || index >= self.size
     count = 0
@@ -66,6 +66,61 @@ class LinkedList
       node = node.next_node
     end
     node
+  end
+
+  # Removes the last element from the list and
+  # returns the popped element (nil if nothing left to pop)
+  def pop
+    if self.size == 0
+      nil
+    elsif self.size == 1
+      node = @head
+      @head = nil
+      @tail = nil
+      node
+    else
+      node = @head
+      until node.next_node == @tail
+        node = node.next_node
+      end
+      node.next_node = nil
+      last_node = @tail
+      @tail = node
+      last_node
+    end
+  end
+
+  # Returns true if the data passed in is in the list and false otherwise
+  def contains?(data)
+    node = @head
+    until node.nil?
+      return true if node.data == data
+      node = node.next_node
+    end
+    false
+  end
+
+  # Returns the index of the node containing data, or nil if not found
+  def find(data)
+    count = 0
+    node = @head
+    until node.nil?
+      return count if node.data == data
+      node = node.next_node
+      count += 1
+    end
+    nil
+  end
+
+  # Returns a string representation of the object
+  def to_s
+    node = @head
+    s = ""
+    until node.nil?
+      s += "( #{node.data} ) -> "
+      node = node.next_node
+    end
+    s += "nil"
   end
 
 end
